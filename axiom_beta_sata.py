89708f96756833bb0c07f7d9189ad293a1c676c9
Florent Kermarrec <florent@enjoy-digital.fr>
2020-11-10 19:59:56 +0100

    rename te0714 to axiom_beta_sata, add the 4 sata ports, test (working) and add test_bist.

--- axiom_beta_sata.py
+++ axiom_beta_sata.py
@@ -12,10 +12,54 @@ _io = [
     ("clk25", 0, Pins("T14"), IOStandard("LVCMOS33")),
 
     # Serial
-    ("serial_usb", 0,
+    ("serial", 0,
         Subsignal("tx", Pins("U12")), # TP7
         Subsignal("rx", Pins("T12")), # TP8
         IOStandard("LVCMOS33")
+    ),
+
+    # Leds
+    ("module_led", 0, Pins("K18"), IOStandard("LVCMOS33")),
+    ("user_led_n", 0, Pins("V12"), IOStandard("LVCMOS33")),
+    ("user_led_n", 1, Pins("U11"), IOStandard("LVCMOS33")),
+    ("user_led_n", 2, Pins("U9"), IOStandard("LVCMOS33")),
+    ("user_led_n", 3, Pins("V11"), IOStandard("LVCMOS33")),
+
+    # SATA
+    ("sata", 0,
+        Subsignal("clk_p", Pins("D6")),
+        Subsignal("clk_n", Pins("D5")),
+        Subsignal("tx_p",  Pins("H2")),
+        Subsignal("tx_n",  Pins("H1")),
+        Subsignal("rx_p",  Pins("E4")),
+        Subsignal("rx_n",  Pins("E3")),
+    ),
+
+    ("sata", 1,
+        Subsignal("clk_p", Pins("D6")),
+        Subsignal("clk_n", Pins("D5")),
+        Subsignal("tx_p",  Pins("F2")),
+        Subsignal("tx_n",  Pins("F1")),
+        Subsignal("rx_p",  Pins("A4")),
+        Subsignal("rx_n",  Pins("A3")),
+    ),
+
+    ("sata", 2,
+        Subsignal("clk_p", Pins("D6")),
+        Subsignal("clk_n", Pins("D5")),
+        Subsignal("tx_p",  Pins("D2")),
+        Subsignal("tx_n",  Pins("D1")),
+        Subsignal("rx_p",  Pins("C4")),
+        Subsignal("rx_n",  Pins("C3")),
+    ),
+
+    ("sata", 3,
+        Subsignal("clk_p", Pins("D6")),
+        Subsignal("clk_n", Pins("D5")),
+        Subsignal("tx_p",  Pins("B2")),
+        Subsignal("tx_n",  Pins("B1")),
+        Subsignal("rx_p",  Pins("G4")),
+        Subsignal("rx_n",  Pins("G3")),
     ),
 ]
 
@@ -50,11 +94,9 @@ import argparse
 
 from migen import *
 
-from litex_boards.platforms import nexys_video
-from litex_boards.targets.nexys_video import _CRG
-
 from litex.build.generic_platform import *
 
+from litex.soc.cores.clock import S7PLL
 from litex.soc.integration.soc_core import *
 from litex.soc.integration.builder import *
 
@@ -66,49 +108,40 @@ from litesata.frontend.bist import LiteSATABIST
 
 from litescope import LiteScopeAnalyzer
 
-# IOs ----------------------------------------------------------------------------------------------
+# CRG ----------------------------------------------------------------------------------------------
 
-_sata_io = [
-    # AB09-FMCRAID / https://www.dgway.com/AB09-FMCRAID_E.html
-    ("fmc2sata", 0,
-        Subsignal("clk_p", Pins("D6")),
-        Subsignal("clk_n", Pins("D5")),
-        Subsignal("tx_p",  Pins("H2")),
-        Subsignal("tx_n",  Pins("H1")),
-        Subsignal("rx_p",  Pins("E4")),
-        Subsignal("rx_n",  Pins("E3"))
-    ),
-]
+class _CRG(Module):
+    def __init__(self, platform, sys_clk_freq):
+        self.clock_domains.cd_sys       = ClockDomain()
+
+        # # #
+
+        self.submodules.pll = pll = S7PLL(speedgrade=-1)
+        pll.register_clkin(platform.request("clk25"), 25e6)
+        pll.create_clkout(self.cd_sys,       sys_clk_freq)
 
 # SATATestSoC --------------------------------------------------------------------------------------
 
 class SATATestSoC(SoCMini):
-    def __init__(self, platform, gen="gen2", with_pll_refclk=True, with_analyzer=False):
-        assert gen in ["gen1", "gen2"]
+    def __init__(self, platform, gen="gen2", with_analyzer=False):
+        assert gen in ["gen1", "gen2", "gen3"]
         sys_clk_freq  = int(100e6)
-        sata_clk_freq = {"gen1": 75e6, "gen2": 150e6}[gen]
+        sata_clk_freq = {"gen1": 75e6, "gen2": 150e6, "gen3": 300e6}[gen]
 
         # CRG --------------------------------------------------------------------------------------
         self.submodules.crg = _CRG(platform, sys_clk_freq)
 
         # SoCMini ----------------------------------------------------------------------------------
         SoCMini.__init__(self, platform, sys_clk_freq,
-            ident         = "LiteSATA bench on Nexys Video",
+            ident         = "LiteSATA bench on Axiom Beta SATA",
             ident_version = True,
             with_uart     = True,
             uart_name     = "bridge")
 
         # SATA -------------------------------------------------------------------------------------
-        if with_pll_refclk:
-            # RefClk, Generate 150MHz from PLL.
-            self.clock_domains.cd_sata_refclk = ClockDomain()
-            self.crg.pll.create_clkout(self.cd_sata_refclk, 150e6)
-            platform.add_platform_command("set_property SEVERITY {{Warning}} [get_drc_checks REQP-49]")
-
         # PHY
         self.submodules.sata_phy = LiteSATAPHY(platform.device,
-            refclk     = None if not with_pll_refclk else self.cd_sata_refclk.clk,
-            pads       = platform.request("fmc2sata"),
+            pads       = platform.request("sata", 0),
             gen        = gen,
             clk_freq   = sys_clk_freq,
             data_width = 16)
@@ -136,17 +169,18 @@ class SATATestSoC(SoCMini):
         # sys_clk
         sys_counter = Signal(32)
         self.sync.sys += sys_counter.eq(sys_counter + 1)
-        self.comb += platform.request("user_led", 0).eq(sys_counter[26])
+        self.comb += platform.request("user_led_n", 0).eq(~sys_counter[26])
         # tx_clk
         tx_counter = Signal(32)
         self.sync.sata_tx += tx_counter.eq(tx_counter + 1)
-        self.comb += platform.request("user_led", 1).eq(tx_counter[26])
+        self.comb += platform.request("user_led_n", 1).eq(~tx_counter[26])
         # rx_clk
         rx_counter = Signal(32)
         self.sync.sata_rx += rx_counter.eq(rx_counter + 1)
-        self.comb += platform.request("user_led", 2).eq(rx_counter[26])
+        self.comb += platform.request("user_led_n", 2).eq(~rx_counter[26])
         # ready
-        self.comb += platform.request("user_led", 3).eq(self.sata_phy.ctrl.ready)
+        self.comb += platform.request("module_led").eq(self.sata_phy.ctrl.ready)
+        self.comb += platform.request("user_led_n", 3).eq(~self.sata_phy.ctrl.ready)
 
         # Analyzer ---------------------------------------------------------------------------------
         if with_analyzer:
@@ -175,17 +209,15 @@ class SATATestSoC(SoCMini):
 # Build --------------------------------------------------------------------------------------------
 
 def main():
-    parser = argparse.ArgumentParser(description="LiteSATA bench on Nexys Video")
+    parser = argparse.ArgumentParser(description="LiteSATA bench on Axiom Beta SATA")
     parser.add_argument("--build",         action="store_true", help="Build bitstream")
     parser.add_argument("--load",          action="store_true", help="Load bitstream (to SRAM)")
-    parser.add_argument("--gen",           default="2",         help="SATA Gen: 1 or 2 (default)")
-    parser.add_argument("--pll-refclk",    action="store_true", help="Generate RefClk from PLL")
+    parser.add_argument("--gen",           default="2",         help="SATA Gen: 1, 2 (default) or 3")
     parser.add_argument("--with-analyzer", action="store_true", help="Add LiteScope Analyzer")
     args = parser.parse_args()
 
     platform = Platform()
-    platform.add_extension(_sata_io)
-    soc = SATATestSoC(platform, "gen" + args.gen, with_pll_refclk=args.pll_refclk, with_analyzer=args.with_analyzer)
+    soc = SATATestSoC(platform, "gen" + args.gen, with_analyzer=args.with_analyzer)
     builder = Builder(soc, csr_csv="csr.csv")
     builder.build(run=args.build)
 
